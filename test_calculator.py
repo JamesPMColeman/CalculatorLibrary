27509120e05033622a444d16f51aaaf71575f8a9
James Coleman <jcolem22@msudenver.edu>
2020-04-12 14:35:20 -0600

    Added a unit test for multiplication

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 42 == calculator.multiply(7, 6)
+
+    def test_division(self):
+        pass
